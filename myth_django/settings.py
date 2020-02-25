b4d248ca2d6758040e43b840962e1d53591be81e
KASSIDY_SHADE <kassie2129@gmail.com>
2020-02-25 09:56:23 -0600

    ok

--- myth_django/settings.py
+++ myth_django/settings.py
@@ -137,12 +137,12 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 PROJECT_ROOT   =   os.path.join(os.path.abspath(__file__))
 STATIC_ROOT  =   os.path.join(PROJECT_ROOT, 'staticfiles')
-STATIC_URL = '../myth/static/'
+# STATIC_URL = '/myth/static/'
 
 # Extra lookup directories for collectstatic to find static files
-STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
-)
+# STATICFILES_DIRS = (
+#     os.path.join(PROJECT_ROOT, 'static'),
+# )
 
 #  Add configuration for static files storage using whitenoise
 # STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
